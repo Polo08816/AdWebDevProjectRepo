abdd1cb721a50caa77490b53b9983a85036ddb37
rajpatel3189 <rajpatel3189@gmail.com>
2016-04-19 13:22:13 -0700

    can can try 1

--- CourseScheduler/db/schema.rb
+++ CourseScheduler/db/schema.rb
@@ -23,20 +23,20 @@ ActiveRecord::Schema.define(version: 20160405211821) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "provider"
-    t.string   "uid"
+    t.text    "email",                  default: "", null: false
+    t.text    "encrypted_password",     default: "", null: false
+    t.text    "reset_password_token"
+    t.text    "reset_password_sent_at"
+    t.text    "remember_created_at"
+    t.integer "sign_in_count",          default: 0,  null: false
+    t.text    "current_sign_in_at"
+    t.text    "last_sign_in_at"
+    t.text    "current_sign_in_ip"
+    t.text    "last_sign_in_ip"
+    t.text    "created_at",                          null: false
+    t.text    "updated_at",                          null: false
+    t.text    "provider"
+    t.text    "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
