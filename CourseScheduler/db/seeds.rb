63563e64996d539583dcfd5dec13e40735dac724
Polo08816 <kkuo12887@gmail.com>
2016-05-03 23:06:23 -0400

    Adding my seed file.

--- CourseScheduler/db/seeds.rb
+++ CourseScheduler/db/seeds.rb
@@ -15,10 +15,10 @@ Course.create(course_name: 'Computer Networks', description: '', course_number: 
 
 # Elective classes
 Course.create(course_name: 'Software Project Management', description: '', course_number: '609', department: "COSC")
-Course.create(course_name: 'Software Engineering', description: '', course_number: '612', department: "COSC", prerequisite_course_number: '600')
-Course.create(course_name: 'Advanced Web Development', description: '', course_number: '617', department: "COSC", prerequisite_course_number: '600')
+Course.create(course_name: 'Software Engineering', description: '', course_number: '612', department: "COSC", prerequisiteCourseNumber: '600')
+Course.create(course_name: 'Advanced Web Development', description: '', course_number: '617', department: "COSC",prerequisiteCourseNumber: '600')
 Course.create(course_name: 'Computer Vision and Image Processing', description: '', course_number: '602', department: "COSC")
 
-Course.create(course_name: 'Software Testing and Maintenance', description: '', course_number: '603', department: "COSC", prerequisite_course_number: '612')
-Course.create(course_name: 'Applied Cryptology', description: '', course_number: '645', department: "COSC", prerequisite_course_number: '600')
-Course.create(course_name: 'Data Mining', description: '', course_number: '757', department: "COSC", prerequisite_course_number: '578')
+Course.create(course_name: 'Software Testing and Maintenance', description: '', course_number: '603', department: "COSC", prerequisiteCourseNumber: '612')
+Course.create(course_name: 'Applied Cryptology', description: '', course_number: '645', department: "COSC", prerequisiteCourseNumber: '600')
+Course.create(course_name: 'Data Mining', description: '', course_number: '757', department: "COSC", prerequisiteCourseNumber: '578')
