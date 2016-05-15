71b6adc8c50a79f0ecc52b09586d786e5e92a8ab
msnyde8 <msnyde8@students.towson.edu>
2016-05-15 12:14:23 -0400

    Included editing of schedules  (mary)

--- CourseScheduler/app/controllers/schedules_controller.rb
+++ CourseScheduler/app/controllers/schedules_controller.rb
@@ -1,5 +1,6 @@
 class SchedulesController < ApplicationController
   load_and_authorize_resource
+#  before_action :set_schedule, only: [:show, :edit, :update, :schedule, :destroy]
 
   def edit
     @schedule = Schedule.find(params[:id])
@@ -12,7 +13,21 @@ class SchedulesController < ApplicationController
   def show
   end
 
+  def update
+    @schedule = Schedule.find(params[:id])
+    respond_to do |format|
+      if @schedule.update(schedule_params)
+        format.html { redirect_to "/users", notice: 'Schedule was successfully updated.' }
+        format.json { render :show, status: :ok, location: @schedule }
+      else
+        format.html { render :edit }
+        format.json { render json: @schedule.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def schedule_params
     params.require(:schedule).permit(:users_id, :courses_id, :semester, :year, :complete, :id)
